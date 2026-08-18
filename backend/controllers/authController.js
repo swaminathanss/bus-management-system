861b0a2ced71a940228f3e272009ef3278e2106a
swaminathanss <swami1203nathan@gmail.com>
2026-08-18 14:25:09 +0530

    backend: auth controller, JWT middleware, auth routes - tested working end-to-end

---
+++ backend/controllers/authController.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  try {
+    const { registerNumber, password, name, mobileNumber, role } = req.body;
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await User.create({ registerNumber, password: hashed, name, mobileNumber, role });
+    res.status(201).json({ id: user._id, registerNumber: user.registerNumber });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { registerNumber, password } = req.body;
+    const user = await User.findOne({ registerNumber });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    res.json({ token, role: user.role, name: user.name });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
